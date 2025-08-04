1b5194feb91767530cb6fedd5588e242faf5e35c
Doraemon33333 <asmasm7789@gmail.com>
2025-08-04 12:50:40 +0900

    day9 Pr

--- src/day9/encapsulation/PhoneStore.java
+++ src/day9/encapsulation/PhoneStore.java
@@ -8,6 +8,12 @@ public class PhoneStore {
 
     }
 
+    // 대리점이 휴대폰을 파는 동작/메서드
+    public Phone sellPhone() {
+        // 대리점이 휴대폰을 판매
+        return phone;
+    }
+
     private void registerPatment() {
         System.out.println("대리점 : 요금제를 등록합니다. 그리고 약정을 등록");
     }
